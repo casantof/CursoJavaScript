8e4d8f36d136bf865f13d555b8b96a29ff0644c8
casantof <casantof@gmail.com>
2022-03-30 20:52:39 -0500

    7. Java es Asincrono?

---
+++ Ejercicio3/js/app.js
@@ -0,0 +1,18 @@
+
+
+
+function imprimir(){
+
+    for(var i = 1; i<=8000; i++){
+
+        console.log("Imprimio");
+    }
+
+}
+
+function presionoClick(){
+
+    console.log("Click en boton");
+}
+
+imprimir();
